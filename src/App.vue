2dff9c45b100384139e2bb9c9ee84777a83a1fa6
eldorado-22 <jumashevv992@gmail.com>
2023-08-25 13:36:28 +0600

    hero_popular_branch

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <script>
 import Homes from "@/components/Homes/homes.vue";
 import {defineComponent} from "vue";
+import Popular from "@/components/Popular_Exersices/popular.vue";
+import {PlayIcon} from "@vue-hero-icons/outline"
+
 
 export default defineComponent({
-  components: {Homes},
+  components: {Homes, Popular},
 })
 
 </script>
 
 <template>
 <Homes/>
+<Popular/>
+<PlayIcon/>
 </template>
 
 <style>
--- src/components/Homes/homes.vue
+++ src/components/Homes/homes.vue
@@ -8,6 +8,7 @@ export default {
       navThree: "pricing",
       navFour: "login",
     }
+
   }
 }
 
